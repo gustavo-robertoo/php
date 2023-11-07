f4ab7f32897383202e003e9716e93bd3ca596f81
gustavo_roberto <145466063+gustavo-robertoo@users.noreply.github.com>
2023-11-07 11:38:36 -0300

    .

---
+++ exp01.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="exp01.css">
+</head>
+<body>
+    <h1> Meu titulo </h1>
+    <p style="background-color: brown"> Olá pessoa </p>
+    
+    <h2>O santos não vai ser rebaixado time grande não cai!!!!!</h2>
+      <p>Isso ai</p>
+</body>
+</html>

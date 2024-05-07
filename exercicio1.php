7c43b14273ff2171b792843e1432382c68a59255
gustavo_roberto <145466063+gustavo-robertoo@users.noreply.github.com>
2024-05-06 22:09:42 -0300

    Update exercicio1.php

--- exercicio1.php
+++ exercicio1.php
@@ -7,9 +7,9 @@
     $varEst = array("Fábio" => "Sao paulo", "Gustavo" => "Bahia", "Guilherme" => "Bahia");
     $varCPF = array("Fábio" => 250, "Gustavo" => 420, "Guilherme" => 785);
 
-    echo "$varAluno[0] tem $varIdade[Fábio] anos de idade. Reside em $varEnd[Fábio], $varCid[Fábio], $varEst[Fábio]. CPF: $varCPF[Fábio]<br>";
-    echo "$varAluno[1] tem $varIdade[Gustavo] anos de idade. Reside em $varEnd[Gustavo], $varCid[Gustavo], $varEst[Gustavo]. CPF: $varCPF[Gustavo]<br>";
-    echo "$varAluno[2] tem $varIdade[Guilherme] anos de idade. Reside em $varEnd[Guilherme], $varCid[Guilherme], $varEst[Guilherme]. CPF: $varCPF[Guilherme]<br>";
+    echo "$varAluno[0] tem $varIdade[Fábio] anos de idade. Reside em $varEnd[Fábio], $varCid[Fábio], $varEst[Fábio]. O seu CPF: $varCPF[Fábio]<br>";
+    echo "$varAluno[1] tem $varIdade[Gustavo] anos de idade. Reside em $varEnd[Gustavo], $varCid[Gustavo], $varEst[Gustavo]. O seu CPF: $varCPF[Gustavo]<br>";
+    echo "$varAluno[2] tem $varIdade[Guilherme] anos de idade. Reside em $varEnd[Guilherme], $varCid[Guilherme], $varEst[Guilherme]. O seu CPF: $varCPF[Guilherme]<br>";
     
     echo "<br>";
 
